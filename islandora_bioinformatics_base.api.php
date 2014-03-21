ac71d83c2d7642dfd9462912865ad98e9bbb60d3
ncphillips <ncphillips.19@gmail.com>
2014-03-21 10:32:08 -0300

    cm_rel_callbacks.inc and base.api.php

---
+++ islandora_bioinformatics_base.api.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file islandora_bioinformatics_base.api.php
+ * This file documents all available hook functions to manipulate data.
+ */
+
+
+function hook_rel_list($object=NULL) {
+
+}
+
+function hook_rel_list_alter($object=NULL) {
+
+}
+
+function hook_rel_SUBTYPE_list_alter($object=NULL) {
+
+}
+
+function hook_rel_add_rel_ui($object=NULL) {
+
+}
+
+function hook_rel_add_rel_ui_alter($object=NULL) {
+
+}
+
+function hook_rel_SUBTYPE_add_rel_ui_alter($object=NULL) {
+
+}
+
+function hook_rel_remove_rel_ui($object=NULL) {
+
+}
+
+function hook_rel_remove_rel_ui_alter($object=NULL) {
+
+}
+
+function hook_rel_SUBTYPE_remove_rel_ui_alter($object=NULL) {
+
+}
