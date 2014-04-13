5830c59cc5c4e067f0a643a51c8949e5e818cbca
ncphillips <ncphillips.19@gmail.com>
2014-04-13 13:54:34 -0300

    related cmodels

--- doxygen/html/search/all_6.js
+++ doxygen/html/search/all_6.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['rel_5fcmodel_5fload',['rel_cmodel_load',['../islandora__bioinformatics__base_8module.html#abaa5d1481d1ed7e3ca134bb436b0c325',1,'islandora_bioinformatics_base.module']]],
+  ['related_5fcmodels',['related_cmodels',['../islandora__bioinformatics__base_8module.html#a042f57d3aea3c501198ded2f31193f6c',1,'islandora_bioinformatics_base.module']]],
   ['related_5fislandora_5fobjects',['related_islandora_objects',['../islandora__bioinformatics__base_8module.html#a1dc5e940beb7a2b30952ce00b01d388e',1,'islandora_bioinformatics_base.module']]],
-  ['related_5fislandora_5fobjects_5fform',['related_islandora_objects_form',['../islandora__bioinformatics__base_8module.html#a746c0835872899ef0101a8a047c8c505',1,'islandora_bioinformatics_base.module']]]
+  ['related_5fislandora_5fobjects_5fform',['related_islandora_objects_form',['../islandora__bioinformatics__base_8module.html#a746c0835872899ef0101a8a047c8c505',1,'islandora_bioinformatics_base.module']]],
+  ['related_5flab_5fobject_5fcmodels',['related_lab_object_cmodels',['../islandora__bioinformatics__base_8module.html#aae0f40af714fe2b8b0435eeabbb00152',1,'islandora_bioinformatics_base.module']]]
 ];
