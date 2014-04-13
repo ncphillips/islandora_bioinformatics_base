2aa5e6728ac200e520366bea957c87263bb78aa8
Islandora User <islandora@upeikerrlab.com>
2014-04-13 18:45:01 -0300

    theming, filled in the templates

--- theme/islandora_person.tpl.php
+++ theme/islandora_person.tpl.php
@@ -7,21 +7,85 @@
  * the islandora_bioinformatics_base_preprocess_islandora_person
  * function.  Elements such as labels and buttons can be added here
  */
- 
- /**
+
+/**
  * 	Written by Quintin Dawson, based off of Islandora_Porcus solution pack
  */
- 
+
 //$islandora_content = $islandora_object['PERSON']->content;
 //$path = drupal_get_path('module', 'islandora_bioinformatics_base');
 
-echo "<pre>";
+/*echo "<pre>";
 print_r($variables["person"]);
-echo "</pre>";
+echo "</pre>";*/
 ?>
 
-<div class="islandora-person-object islandora">
-  <div class ="person_image">
-    <?php print $variables['islandora_thumbnail_img'] ?>
-  </div>
+<style>
+    .person-info {
+        float: left;
+        position: relative;
+        top: 8px;
+        margin-right: 20px;
+    }
+
+    .person-pic {
+        float: left;
+        position: relative;
+        top: 8px;
+        border: none;
+    }
+
+    .person-info table {
+        border: none;
+    }
+
+    .person-info-label {
+        font-weight: bold;
+        font-size: 14px;
+        width: 180px;
+    }
+
+    .person-info-heading {
+        font-weight: bold;
+        font-size: 14px;
+        width: 180px;
+        border: none;
+        background: none;
+        text-decoration:underline;
+    }
+</style>
+
+<div class="person-info">
+    <table>
+        <tbody>
+        <tr><td><img src="<?php print $variables['img_url'] ?>" width="250"></td></tr>
+        </tbody>
+    </table>
 </div>
+<div class="person-info">
+    <table>
+        <tbody>
+        <tr><th class="person-info-heading">Personal Information</th></tr>
+        <tr><td class="person-info-label">Title:</td><td><?php print $variables['person']['title'] ?></td></tr>
+        <tr><td class="person-info-label">First Name:</td><td><?php print $variables['person']['name']['first'] ?></td></tr>
+        <tr><td class="person-info-label">Last Name:</td><td><?php print $variables['person']['name']['last'] ?></td></tr>
+        <tr><td class="person-info-label">Birth Date: Name:</td><td><?php print $variables['person']['name']['birthdate'] ?></td></tr>
+        </tbody>
+    </table>
+</div>
+<div class="person-info">
+    <table>
+        <tbody>
+        <tr><th class="person-info-heading">Contact Information</th></tr>
+        <tr><td class="person-info-label">Phone Number:</td><td><?php print $variables['person']['contact']['phone'] ?></td></tr>
+        <tr><td class="person-info-label">Email Address:</td><td><?php print $variables['person']['contact']['email'] ?></td></tr>
+        <tr><td class="person-info-label">Organization:</td><td><?php print $variables['person']['contact']['organization'] ?></td></tr>
+        <tr><td class="person-info-label">Position:</td><td><?php print $variables['person']['contact']['position'] ?></td></tr>
+        <tr><td class="person-info-label">Address:</td><td><?php print $variables['person']['contact']['address'] ?></td></tr>
+        <tr><td class="person-info-label">Start Date:</td><td><?php print $variables['person']['contact']['startDate'] ?></td></tr>
+        <tr><td class="person-info-label">End Date:</td><td><?php print $variables['person']['contact']['endDate'] ?></td></tr>
+        <tr><td class="person-info-label">Website:</td><td><?php print $variables['person']['contact']['url'] ?></td></tr>
+        </tbody>
+    </table>
+</div>
+
--- theme/islandora_project.tpl.php
+++ theme/islandora_project.tpl.php
@@ -15,13 +15,67 @@
 //$islandora_content = $islandora_object['PERSON']->content;
 //$path = drupal_get_path('module', 'islandora_bioinformatics_base');
 
-echo "<pre>";
+/*echo "<pre>";
 print_r($variables['project']);
-echo "</pre>";
+echo "</pre>";*/
 ?>
 
-<div class="islandora-person-object islandora">
-    <div class ="person_image">
-        <?php print $variables['islandora_thumbnail_img'] ?>
-    </div>
+<style>
+    .person-info {
+        float: left;
+        position: relative;
+        top: 8px;
+        margin-right: 20px;
+    }
+
+    .person-pic {
+        float: left;
+        position: relative;
+        top: 8px;
+        border: none;
+    }
+
+    .person-info table {
+        border: none;
+    }
+
+    .person-info-label {
+        font-weight: bold;
+        font-size: 14px;
+        width: 180px;
+    }
+
+    .person-info-heading {
+        font-weight: bold;
+        font-size: 14px;
+        width: 180px;
+        border: none;
+        background: none;
+        text-decoration:underline;
+    }
+</style>
+
+<div class="person-info">
+    <table>
+        <tbody>
+        <tr><td><img src="<?php print $variables['img_url'] ?>" width="250"></td></tr>
+        </tbody>
+    </table>
+</div>
+<div class="person-info">
+    <table>
+        <tbody>
+        <tr><th class="person-info-heading">Project Information</th></tr>
+        <tr><td class="person-info-label">Title:</td><td><?php print $variables['project']['title'] ?></td></tr>
+        <tr><td class="person-info-label">Organization:</td><td><?php print $variables['project']['organization'] ?></td></tr>
+        <tr><td class="person-info-label">City:</td><td><?php print $variables['project']['address']['city'] ?></td></tr>
+        <tr><td class="person-info-label">Street:</td><td><?php print $variables['project']['address']['street'] ?></td></tr>
+        <tr><td class="person-info-label">Country:</td><td><?php print $variables['project']['address']['country'] ?></td></tr>
+        <tr><td class="person-info-label">Postal Code:</td><td><?php print $variables['project']['address']['postcode'] ?></td></tr>
+        <tr><td class="person-info-label">Email Address:</td><td><?php print $variables['project']['email'] ?></td></tr>
+        <tr><td class="person-info-label">Phone Number:</td><td><?php print $variables['project']['phone'] ?></td></tr>
+        <tr><td class="person-info-label">Start Date:</td><td><?php print $variables['project']['startdate'] ?></td></tr>
+        <tr><td class="person-info-label">End Date:</td><td><?php print $variables['project']['enddate'] ?></td></tr>
+        </tbody>
+    </table>
 </div>
